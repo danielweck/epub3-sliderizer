1e1103cb245598886fdb3115c618a6f12ae51a50
Daniel Weck <daniel.weck@gmail.com>
2013-06-25 19:19:53 +0100

    content update

--- thin.sh
+++ thin.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-thin -c _OUTPUT/content/EPUB3/epub/ -p 3000 -A file start &
+thin -c _OUTPUT/content/ -p 3000 -A file start &
 
-open http://localhost:3000/Nav.xhtml
+open http://localhost:3000/EPUB3/epub/Nav.xhtml
