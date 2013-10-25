8fa1b35ced45117100f74d9731f621a739ebf74b
Daniel Weck <daniel.weck@gmail.com>
2013-10-25 14:13:39 +0100

    www up

--- git.sh
+++ git.sh
@@ -3,9 +3,17 @@
 #git checkout gh-pages
 
 cp _OUTPUT/content/EPUB3-Sliderizer.epub .
+
 cp -r _OUTPUT/content/EPUB3/* doc/
 
-git status
+git status --short doc
+
+git ls-files --exclude-standard --others doc | xargs git add
+
+git status --short doc
+
+root=$(pwd)
+echo "${root}"
 
 git commit -a -m "www up"
 git push
