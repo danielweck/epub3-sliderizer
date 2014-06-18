6e67d7839adc6d0bf969a75f48cc84fe75d21f5f
Daniel Weck <daniel.weck@gmail.com>
2014-06-18 10:49:33 +0100

    scroll fixed

--- thin.sh
+++ thin.sh
@@ -2,8 +2,8 @@
 
 # killall thin
 
-thin -c _OUTPUT/ -p 3000 -a 192.168.1.68 -A file start &
+thin -c _OUTPUT/ -p 3333 -a 192.168.1.68 -A file start &
 
-open http://192.168.1.68:3000/content/EPUB3/epub/nav.xhtml
+open http://192.168.1.68:3333/content/EPUB3/epub/nav.xhtml
 
 #open http://localhost:3000/content/index.html
